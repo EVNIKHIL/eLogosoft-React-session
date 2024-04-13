000352b5e6a9532d64f1774c47e2daec04173c4e
Nikhil EV <evnikhilpba@gmail.com>
2024-04-13 16:48:02 +0530

    Formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import FormikComponent from "./Formik";
 
 function App() {
   return (
     <>
       <h1>Hello react</h1>
+      <FormikComponent />
     </>
   );
 }
---
+++ src/FormikHook.jsx
@@ -0,0 +1,27 @@
+import { useFormik } from "formik";
+
+const FormikHook = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormikHook;
