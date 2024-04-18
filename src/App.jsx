3f7f8a9c83ee3d4110a561a965073673dea167ac
Nikhil EV <evnikhilpba@gmail.com>
2024-04-18 13:41:46 +0530

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import "./App.css";
+import { TodoList } from "./TodoList";
+import { createTodos } from "./utils";
+
+const todos = createTodos();
 
 function App() {
+  const [tab, setTab] = useState("all");
+  const [isDark, setIsDark] = useState(false);
   return (
     <>
       <h1>Hello react</h1>
+      <>
+        <button
+          className={`${tab === "all" ? "selected" : ""}`}
+          onClick={() => setTab("all")}
+        >
+          All
+        </button>
+        <button
+          className={`${tab === "active" ? "selected" : ""}`}
+          onClick={() => setTab("active")}
+        >
+          Active
+        </button>
+        <button
+          className={`${tab === "completed" ? "selected" : ""}`}
+          onClick={() => setTab("completed")}
+        >
+          Completed
+        </button>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            checked={isDark}
+            onChange={(e) => setIsDark(e.target.checked)}
+          />
+          Dark mode
+        </label>
+        <hr />
+        <TodoList todos={todos} tab={tab} theme={isDark ? "dark" : "light"} />
+      </>
     </>
   );
 }
